e112f25440f9268216dcbc7067391b1302b35383
pajic <32738446+pajicadvance@users.noreply.github.com>
2026-01-19 00:08:56 +0100

    Better pausing

--- src/main/java/me/pajic/plugin/ServerPausePlugin.java
+++ src/main/java/me/pajic/plugin/ServerPausePlugin.java
@@ -2,10 +2,12 @@ package me.pajic.plugin;
 
 import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.logger.HytaleLogger;
+import com.hypixel.hytale.server.core.event.events.BootEvent;
 import com.hypixel.hytale.server.core.event.events.player.PlayerConnectEvent;
 import com.hypixel.hytale.server.core.event.events.player.PlayerDisconnectEvent;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+import com.hypixel.hytale.server.core.universe.Universe;
 import com.hypixel.hytale.server.core.universe.world.World;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 
@@ -22,6 +24,15 @@ public class ServerPausePlugin extends JavaPlugin {
 
     @Override
     protected void setup() {
+        // pause worlds when server starts
+        getEventRegistry().register(BootEvent.class, event -> {
+            Universe.get().getWorlds().values().forEach(world -> {
+                LOGGER.atInfo().log("Paused world %s", world.getName());
+                world.setPaused(true);
+            });
+            LOGGER.atInfo().log("Server started, paused all worlds.");
+        });
+        // pause world when last player disconnects
         getEventRegistry().register(PlayerDisconnectEvent.class, event -> {
             Ref<EntityStore> storeRef = event.getPlayerRef().getReference();
             if (storeRef != null && storeRef.isValid()) {
@@ -32,11 +43,12 @@ public class ServerPausePlugin extends JavaPlugin {
                 }
             }
         });
+        // unpause world when player connects and world is paused
         getEventRegistry().register(PlayerConnectEvent.class, event -> {
             World world = event.getWorld();
             if (world != null && world.isPaused()) {
                 world.setPaused(false);
-                LOGGER.atInfo().log("Resumed world.");
+                LOGGER.atInfo().log("Player joined, resumed world.");
             }
         });
     }
