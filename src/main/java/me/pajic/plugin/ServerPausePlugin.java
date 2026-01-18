8d88df262c45cad08618da6b745b13300b7107a8
pajic <32738446+pajicadvance@users.noreply.github.com>
2026-01-18 23:53:53 +0100

    Server pause mod

---
+++ src/main/java/me/pajic/plugin/ServerPausePlugin.java
@@ -0,0 +1,43 @@
+package me.pajic.plugin;
+
+import com.hypixel.hytale.component.Ref;
+import com.hypixel.hytale.logger.HytaleLogger;
+import com.hypixel.hytale.server.core.event.events.player.PlayerConnectEvent;
+import com.hypixel.hytale.server.core.event.events.player.PlayerDisconnectEvent;
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+import com.hypixel.hytale.server.core.universe.world.World;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
+
+import javax.annotation.Nonnull;
+
+@SuppressWarnings("unused")
+public class ServerPausePlugin extends JavaPlugin {
+
+    private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
+
+    public ServerPausePlugin(@Nonnull JavaPluginInit init) {
+        super(init);
+    }
+
+    @Override
+    protected void setup() {
+        getEventRegistry().register(PlayerDisconnectEvent.class, event -> {
+            Ref<EntityStore> storeRef = event.getPlayerRef().getReference();
+            if (storeRef != null && storeRef.isValid()) {
+                World world = storeRef.getStore().getExternalData().getWorld();
+                if (world.getPlayerCount() == 1) {
+                    world.setPaused(true);
+                    LOGGER.atInfo().log("No players online, paused world.");
+                }
+            }
+        });
+        getEventRegistry().register(PlayerConnectEvent.class, event -> {
+            World world = event.getWorld();
+            if (world != null && world.isPaused()) {
+                world.setPaused(false);
+                LOGGER.atInfo().log("Resumed world.");
+            }
+        });
+    }
+}
